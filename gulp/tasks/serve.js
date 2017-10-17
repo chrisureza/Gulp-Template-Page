f97d689c2f4604e61eb3ce1c5bfcfd1b0055e5d6
Christian Urena <chrurena@crsjopgp0341.global.publicisgroupe.net>
2017-10-17 09:41:01 -0600

    chrisureza - update browserSync reload

--- gulp/tasks/serve.js
+++ gulp/tasks/serve.js
@@ -1,10 +1,20 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
 
+
+gulp.task('browserSync', () => {
+    browserSync.init({
+        server: {
+            baseDir: 'app/partial_build'
+        }
+    });
+});
+
 gulp.task('serve', ['browserSync', 'sass', 'babel'], () => {
     gulp.watch('app/development/scss/**/*.scss', ['sass']);
     gulp.watch('app/development/es6/**/*.js', ['babel']);
     gulp.watch('app/development/pug/**/*.pug', ['pug']);
     gulp.watch('app/partial_build/**/*.html', browserSync.reload);
     gulp.watch('app/partial_build/js/**/*.js', browserSync.reload);
+    gulp.watch('app/partial_build/css/**/*.css', browserSync.reload)
 });
